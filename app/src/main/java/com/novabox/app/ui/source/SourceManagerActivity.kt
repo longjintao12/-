db68a626e11daa0cbe78c834a7b2514a35ad8e22
longjintao12 <3513628293@qq.com>
2026-08-21 03:11:57 +0800

    fix: 修复 Var 不能重新赋值

--- app/src/main/java/com/novabox/app/ui/source/SourceManagerActivity.kt
+++ app/src/main/java/com/novabox/app/ui/source/SourceManagerActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.novabox.app.R
